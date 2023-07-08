78af0f1476b052bc7236b57fcd0668c981d5a2f2
eranandsharma <138469547+eranandsharma@users.noreply.github.com>
2023-07-08 23:11:07 +0545

    3rd
    3rd

--- script.js
+++ script.js
@@ -2,14 +2,56 @@
 const navbar = document.querySelector('.navbar');
 
         window.addEventListener('scroll', () => {
-            if (window.scrollY > 20) {
+            if (window.scrollY > 1) {
                 navbar.classList.add('sticky');
             } else {
                 navbar.classList.remove('sticky');
             }
+            if(this.scrollY>500){
+                $('.scroll-up-btn').addClass("show");
+            }else{
+                $('.scroll-up-btn').removeClass("show");   
+            }
+            
+        });
+
+        $('.scroll-up-btn').click(function(){
+            $('html').animate({scrollTop:0 });
+            $('html').css("scrollBehavior","auto");
+        });
+        $('.navbar .menu li a').click(function(){
+            $('html').css("scrollBehavior","smooth");
         });
 
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
-    })
+    });
+
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut:2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+
+    });
+
+    
+    
+    
