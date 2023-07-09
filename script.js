6d53ecd7111e9d1ef36a1cd4c1c7d8abd70423dc
eranandsharma <138469547+eranandsharma@users.noreply.github.com>
2023-07-09 15:16:07 +0545

    4th
    4th

--- script.js
+++ script.js
@@ -7,18 +7,9 @@ const navbar = document.querySelector('.navbar');
             } else {
                 navbar.classList.remove('sticky');
             }
-            if(this.scrollY>500){
-                $('.scroll-up-btn').addClass("show");
-            }else{
-                $('.scroll-up-btn').removeClass("show");   
-            }
-            
         });
 
-        $('.scroll-up-btn').click(function(){
-            $('html').animate({scrollTop:0 });
-            $('html').css("scrollBehavior","auto");
-        });
+
         $('.navbar .menu li a').click(function(){
             $('html').css("scrollBehavior","smooth");
         });
